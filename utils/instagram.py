45b907f3dfe5689fcd951ccf8a8a255ec79d4163
NasirKhalid24 <nasirmkhalid24@gmail.com>
2022-01-30 06:57:56 -0500

    reduce time

--- utils/instagram.py
+++ utils/instagram.py
@@ -14,11 +14,9 @@ def login(driver, username_input, password_input):
 
     driver.get("http://www.instagram.com")
 
-    time.sleep(1)
-
     #target username
-    username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+    username = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+    password = WebDriverWait(driver, 0.5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
 
     #enter username and password
     username.clear()
@@ -29,12 +27,8 @@ def login(driver, username_input, password_input):
     #target the login button and click it
     button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
 
-    not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
-    not_now2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
-
-
-    time.sleep(1.5)
-    
+    not_now = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+    not_now2 = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()    
 
 def get_elements(driver):
     elements = driver.find_elements_by_xpath("//article[@class='_8Rm4L bLWKA M9sTE _1gNme h0YNM  SgTZ1    ']")
@@ -42,7 +36,6 @@ def get_elements(driver):
 
 def get_screenshots(driver, username, password):
     login(driver, username, password)
-    time.sleep(1)
     elements = get_elements(driver)
     for i,elem in enumerate(elements):
         driver.execute_script("return arguments[0].scrollIntoView(false);", elem)
